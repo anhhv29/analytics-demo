cda1203c00e444174518719d60271a8a7fd0267e
anhhv <anhhv@cscmobi.com>
2024-09-30 19:42:25 +0700

    feat: update native inline

--- app/src/main/java/com/example/analytics1/view/activity/native_ad/native_inline/NativeInlineAdsActivity.kt
+++ app/src/main/java/com/example/analytics1/view/activity/native_ad/native_inline/NativeInlineAdsActivity.kt
@@ -1,31 +1,41 @@
 package com.example.analytics1.view.activity.native_ad.native_inline
 
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.analytics1.base.activity.BaseActivity
 import com.example.analytics1.databinding.ActivityNativeInlineAdsBinding
 import com.example.analytics1.model.ItemDemoModel
 import com.example.analytics1.model.data.ItemDemoProvider
 import com.example.analytics1.view.adapter.NativeInlineAdsAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class NativeInlineAdsActivity : BaseActivity<ActivityNativeInlineAdsBinding>() {
     override fun getActivityBinding() = ActivityNativeInlineAdsBinding.inflate(layoutInflater)
     private var mAdapter: NativeInlineAdsAdapter? = null
     override fun initView() {
         super.initView()
-        val dataItemDemo = ItemDemoProvider().getListNativeInlineDemo()
-        mAdapter = NativeInlineAdsAdapter(this, dataItemDemo)
+
+        mAdapter = NativeInlineAdsAdapter(this, mutableListOf())
+
+        val mLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rcvNativeInlineDemo.apply {
+            adapter = mAdapter
+            layoutManager = mLayoutManager
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val dataItemDemo = ItemDemoProvider().getListNativeInlineDemo()
+            withContext(Dispatchers.Main) {
+                mAdapter?.updateData(dataItemDemo)
+            }
+        }
 
         mAdapter?.setClickListener(object : NativeInlineAdsAdapter.ItemClickListener {
             override fun eventClick(itemDemoModel: ItemDemoModel) {
 
             }
         })
-
-        val mLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-
-        binding.rcvNativeInlineDemo.apply {
-            adapter = mAdapter
-            layoutManager = mLayoutManager
-        }
     }
 }
--- app/src/main/java/com/example/analytics1/view/adapter/NativeInlineAdsAdapter.kt
+++ app/src/main/java/com/example/analytics1/view/adapter/NativeInlineAdsAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.analytics1.view.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
@@ -10,67 +11,136 @@ import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import com.example.analytics1.databinding.ItemDemoBinding
+import com.example.analytics1.R
+import com.example.analytics1.ads.NativeInlineManager
+import com.example.analytics1.databinding.ItemDemoNativeInlineBinding
 import com.example.analytics1.model.ItemDemoModel
 import com.example.analytics1.util.MyUtils.Companion.goneView
+import com.example.analytics1.util.MyUtils.Companion.visibleView
+import com.example.analytics1.util.SharedPreferences
 
 class NativeInlineAdsAdapter(
     private var context: Context,
     originList: MutableList<ItemDemoModel>
 ) : RecyclerView.Adapter<NativeInlineAdsAdapter.ViewHolder>() {
-    private lateinit var itemClickListener: ItemClickListener
-    private var mList = originList
+    private var mList: MutableList<ItemDemoModel> = resolveAds(originList)
+    private var nativeManager: NativeInlineManager? = null
+    private val isPro: Boolean = SharedPreferences.isProApp(context)
+    private var itemClickListener: ItemClickListener? = null
 
-    class ViewHolder(val binding: ItemDemoBinding) : RecyclerView.ViewHolder(binding.root)
+    class ViewHolder(val binding: ItemDemoNativeInlineBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding =
-            ItemDemoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ItemDemoNativeInlineBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, itemPosition: Int) {
+        val itemDemoModel: ItemDemoModel = mList[itemPosition]
+
         holder.binding.apply {
-            Glide.with(context).load(mList[itemPosition].image)
-                .addListener(object : RequestListener<Drawable> {
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<Drawable>,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        vLoading.goneView()
-                        return false
-                    }
+            if (shouldShowAd(itemDemoModel)) {
+                showAd(holder, itemDemoModel)
+            } else {
+                showContent()
+            }
 
-                    override fun onResourceReady(
-                        resource: Drawable,
-                        model: Any,
-                        target: Target<Drawable>,
-                        dataSource: DataSource,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        vLoading.goneView()
-                        return false
-                    }
-
-                }).into(image)
+            loadImage(itemDemoModel.image)
 
             image.setOnClickListener {
-                itemClickListener.eventClick(mList[itemPosition])
+                itemClickListener?.eventClick(itemDemoModel)
             }
         }
+    }
+
+    private fun ItemDemoNativeInlineBinding.showAd(
+        holder: ViewHolder,
+        itemDemoModel: ItemDemoModel
+    ) {
+        nativeManager =
+            NativeInlineManager.newInstance(context, context.getString(R.string.native_ad_unit_id))
+        layoutView.goneView()
+        layoutAds.visibleView()
+
+        nativeManager?.loadNativeAd { nativeAd ->
+            if (nativeAd != null) {
+                nativeManager?.loadAdTemplate(
+                    nativeAd,
+                    R.layout.native_ad_template_banner,
+                    holder.binding.layoutAds
+                )
+            } else {
+                removeAd(holder, itemDemoModel)
+            }
+        }
+    }
+
+    private fun removeAd(holder: ViewHolder, itemDemoModel: ItemDemoModel) {
+        notifyItemRemoved(holder.adapterPosition)
+        mList.remove(itemDemoModel)
+        holder.binding.layoutAds.goneView()
+    }
+
+    private fun ItemDemoNativeInlineBinding.showContent() {
+        layoutView.visibleView()
+        layoutAds.goneView()
+    }
+
+    private fun ItemDemoNativeInlineBinding.loadImage(imageData: Int) {
+        Glide.with(context).load(imageData)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    vLoading.goneView()
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable,
+                    model: Any,
+                    target: Target<Drawable>,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    vLoading.goneView()
+                    return false
+                }
+            }).into(image)
     }
 
     override fun getItemCount(): Int {
         return mList.size
     }
 
-    interface ItemClickListener {
-        fun eventClick(itemDemoModel: ItemDemoModel)
-    }
-
     fun setClickListener(clickListener: ItemClickListener) {
         this.itemClickListener = clickListener
+    }
+
+    private fun resolveAds(data: MutableList<ItemDemoModel>): MutableList<ItemDemoModel> {
+        if (isPro) return data
+        return data.flatMapIndexed { index, item ->
+            if (index > 0 && index % 4 == 0) listOf(
+                ItemDemoModel(type = "ADS_INLINE"),
+                item
+            ) else listOf(item)
+        }.toMutableList()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateData(data: MutableList<ItemDemoModel>) {
+        mList = resolveAds(data)
+        notifyDataSetChanged()
+    }
+
+    private fun shouldShowAd(item: ItemDemoModel) = !isPro && item.type == "ADS_INLINE"
+
+    interface ItemClickListener {
+        fun eventClick(itemDemoModel: ItemDemoModel)
     }
 }
